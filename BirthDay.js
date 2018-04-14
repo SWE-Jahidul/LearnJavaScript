afa1ee3bf07de90e895304a7a8a7c74ca39b1fc5
jahidul <you@example.com>
2018-04-14 21:41:04 +0600

    Adding 25 file and one is function and another is birthday Gift

---
+++ BirthDay.js
@@ -0,0 +1,18 @@
+var name = "Mamnun";
+var i =0;
+while(i<2)
+{
+      console.log("Happy Birth Day");
+      i=i+1;   
+}
+console.log("Happy Birth Day My Dear "+ name);
+console.log("Happy Birth Day To You");
+
+
+
+
+
+
+
+
+
---
+++ Function.js
@@ -0,0 +1,69 @@
+//-----------------------------------------Function----------------------------------------
+
+// Defineing JavaScript Function 
+
+// function hello()
+
+// {
+//     console.log("hellow JavaScript Function World");
+    
+// }
+
+// hello();
+
+//######################################### Return Function ##############################
+
+
+// var Square = function(x)
+// {
+//     return x*x;  
+// }
+
+// console.log(Square(10));
+
+
+
+//#################################### Paramiter Function ################################### 
+
+
+// function Distance(x1,y1,x2,y2)
+// {
+// var dx =x2-x1;
+// var dy =y2-y1;
+// return Math.sqrt(dx*dx +dy*dy)
+// }
+// console.log(Distance(2,3,4,5));
+
+
+
+//####################################### Nasted Function ##################################
+
+
+// function firstFunction (a, b)
+// {
+//     function Square(x)
+//     {
+//       return x*x;
+//     }
+   
+//     //return a*b;
+//     return Math.sqrt(Square (a) * Square (b));
+
+//    }
+// console.log(firstFunction(2,6))
+
+
+//################################## Invoked Function #####################################
+
+
+
+
+
+
+
+
+
+
+
+
+
